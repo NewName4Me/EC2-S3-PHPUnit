69a14c9d6fe8815cb92bc7d22f2c85fe3f4641f7
NewName4ME <tortarod@gmail.com>
2024-11-18 00:51:03 +0100

    cambio orden politicas

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_public_access_block" "s3_public_block" {
   bucket = aws_s3_bucket.s3.id
 
   block_public_acls       = false
-  block_public_policy     = false
+  block_public_policy     = false 
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
